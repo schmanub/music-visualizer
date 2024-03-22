1ee8d8832ba822521f3cc6d7085fcb3dfd71567b
Schmanub <asmanu21@gmail.com>
2024-03-21 20:28:27 -0400

    Initial code that imports pygame and creates a window

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pygame as pg
+import sys
 
+# Manuel Marchand, Ethan Dunn
+
+pg.init()
+
+# setup window
+screen = pg.display.set_mode((500, 500))
+pg.display.set_caption("Music Visualizer")
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
