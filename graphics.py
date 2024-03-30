93ecc65105ef09ef78101229a7677b00d095eb0f
ethdunn <edunn4233@gmail.com>
2024-03-30 18:53:39 -0400

    Added changes to graphics. cleaned code

--- graphics.py
+++ graphics.py
@@ -1,1 +1,38 @@
+import pygame as pg
+from numpy.fft import fft
+import numpy as np
 
+
+def map_range(range1: tuple, range2: tuple, value):
+    slope = (range2[1] - range2[0]) / (range1[1] - range1[0])
+    return range2[0] + slope * (value - range1[0])
+
+
+class Waveform:
+    def __init__(self, screen, spect_data, color):
+        self.screen = screen
+        self.spect_data = spect_data
+        self.color = color
+        self.rect_count = 128
+
+    def line(self):
+        xy = list()
+        length = len(self.spect_data)
+        interval = self.screen.get_width() / length
+        # rectangle drawing part
+        for i in range(0, length):
+            value = map_range((-32768, 32767), (0, self.screen.get_height()), self.spect_data[i])
+            xy.append((interval*i, value))
+        pg.draw.lines(self.screen, self.color, False, xy, 2)
+
+    def histogram(self):
+        space = 5
+        interval = self.screen.get_width() / self.rect_count
+        interval2 = ((self.screen.get_width()) / (self.rect_count + space))
+        # use fourier transforms to visualize the data
+        data = abs(fft(self.spect_data, self.rect_count, -1, "ortho"))
+        normalized_data = data / np.linalg.norm(data)
+        for i in range(0, self.rect_count):
+            height = normalized_data[i] * (self.screen.get_height() * 2)
+            rect = (i * (interval + space), self.screen.get_height() - height, interval2, height)
+            pg.draw.rect(self.screen, self.color, rect)
--- main.py
+++ main.py
@@ -6,25 +6,31 @@ import numpy as np
 import pygame as pg
 from pygame import mixer
 
+# graphics.py file
+import graphics
+
 # Manuel Marchand, Ethan Dunn
 
+
 pg.init()
-mixer.init()
+
+# ensure a sound device is connected
+while True:
+    try:
+        mixer.init()
+        break
+    except:
+        print("NO SOUND DEVICE FOUND")
 
 # setup window
 SCREEN_WIDTH = 1500
 SCREEN_HEIGHT = 300
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pg.RESIZABLE)
 pg.display.set_caption("Music Visualizer")
-text_font = pg.font.SysFont("arial", 20)
+text_font = pg.font.SysFont("arial", 16)
 clock = pg.time.Clock()
 frame_rate = 60
 mixer.music.set_volume(0.3)
-
-
-def map_range(range1: tuple, range2: tuple, value):
-    slope = (range2[1] - range2[0]) / (range1[1] - range1[0])
-    return range2[0] + slope * (value - range1[0])
 
 
 def draw_text(text, font, color, x, y):
@@ -38,37 +44,12 @@ def end(file_stream):
     sys.exit()
 
 
-def rgb():
-    m = .5
-    x = pg.time.get_ticks() / 500
-    return int(255 * (m * (math.cos(x)) + m)), int(255 * (m * (math.cos(x + (math.pi * (2 / 3)))) + m)), int(255 * (
-            m * (math.cos(x + (math.pi * (4 / 3)))) + m))
-
-
-def wave_form(spect_data):
-    rect_count = len(spect_data)
-    interval = screen.get_width() / rect_count
-    # rectangle drawing part
-    for i in range(0, rect_count):
-        value = map_range((-32768, 32768), (0, 1), spect_data[i])
-        height = value * screen.get_height()
-        rect = (i * interval, (screen.get_height() - height) / 2, interval, height)
-        pg.draw.rect(screen, rgb(), rect)
-
-def wave_form2(spect_data):
-    rect_count = len(spect_data)
-    interval = screen.get_width() / rect_count
-    # rectangle drawing part
-    for i in range(0, rect_count):
-        value = map_range((-32768, 32768), (0, 1), spect_data[i])
-        height = value * screen.get_height()
-        rect = (i * interval, height, interval, 10)
-        pg.draw.rect(screen, rgb(), rect)
-
-
-def frequency_analyzer(spect_data, rect_count): \
-        # bin_width = 65536 / rect_count
-    pass
+def rgb(time):
+    # 3 cos waves all 120 degrees out of phase with each other
+    x = time / 500
+    return (int(255 * (0.5 * (math.cos(x)) + 0.5)),
+            int(255 * (0.5 * (math.cos(x + (math.pi * (2 / 3)))) + 0.5)),
+            int(255 * (0.5 * (math.cos(x + (math.pi * (4 / 3)))) + 0.5)))
 
 
 def main_menu():
@@ -78,7 +59,7 @@ def main_menu():
         # handle text
         screen.fill("white")
         # centered rectangle with text inside, ensures text is always centered
-        text = text_font.render("Drag a .wav into this window", True, rgb())
+        text = text_font.render("Drag a .wav into this window", True, rgb(pg.time.get_ticks()))
         text_rect = text.get_rect(center=(screen.get_width() / 2, screen.get_height() / 2))
         screen.blit(text, text_rect)
         # check for events
@@ -89,16 +70,16 @@ def main_menu():
             # a file has been dragged into the window
             elif event.type == pg.DROPFILE:
                 print("File path: ", event.file)
-                if not event.file.endswith(".wav"):
-                    message = True
-                    message_start = pg.time.get_ticks()
-                else:
+                if event.file.endswith(".wav"):
                     mixer.music.load(event.file)
                     wav_header(event.file)
+                else:
+                    message = True
+                    message_start = pg.time.get_ticks()
         clock.tick()
         if message:
             # draw error on screen for 1000 ticks
-            draw_text("Error", text_font, "red", 10, 40)
+            draw_text("Wrong file type", text_font, "red", 10, 40)
             if pg.time.get_ticks() > message_start + 1000:
                 message = False
         pg.display.update()
@@ -109,6 +90,7 @@ def wav_header(input_file):
     file_stream = open(input_file, "rb")
     header_meaning = ["RIFF", "ChunkSize", "WAVE", "fmt ", "Sub chunk1size", "audio format", "channel count",
                       "samplerate", "byte rate", "block align", "bits per sample", "data", "sub chunk 2 size"]
+    # H = short int, I = long int
     header_data = struct.unpack("<IIIIIHHIIHHII", file_stream.read(44))
     # check if the file is a 16 bit pcm wave file
     if header_data[0] == 1179011410 and header_data[2] == 1163280727 and header_data[10] == 16:
@@ -126,38 +108,39 @@ def wav_header(input_file):
 def visualizer(file_stream, block_size):
     graphic_screen = 0
     vol = 0.3
-    rect_count = 128
     mixer.music.play()
     while True:
         mixer.music.set_volume(vol)
         data = file_stream.read(block_size)
         decoded_data = np.frombuffer(data, dtype=np.int16)
         screen.fill((0, 0, 0))
+        draw_text(str(clock.get_fps()), text_font, "white", 10, 10)
         if len(decoded_data) == 0:
             file_stream.close()
             main_menu()
+        wave_form = graphics.Waveform(screen, decoded_data, rgb(pg.time.get_ticks()))
         match graphic_screen:
             case 0:
-                wave_form(decoded_data)
+                wave_form.histogram()
             case 1:
-                wave_form2(decoded_data)
+                wave_form.line()
         clock.tick(frame_rate)
         pg.display.update()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 end(file_stream)
-            elif event.type == pg.KEYDOWN:
+            if event.type == pg.KEYDOWN:
                 if event.key == pg.K_DOWN:
                     vol -= 0.1
-                elif event.key == pg.K_UP:
+                if event.key == pg.K_UP:
                     vol += 0.1
-                elif event.key == pg.K_LEFT:
-                    print("left")
-                    if graphic_screen > 0:
-                        graphic_screen -= 1
-                elif event.key == pg.K_RIGHT:
-                    print("right")
-                    if graphic_screen < 1:
-                        graphic_screen += 1
+                if event.key == pg.K_SPACE:
+                    if graphic_screen == 0:
+                        graphic_screen = 1
+                    else:
+                        graphic_screen = 0
+            if event.type == pg.VIDEOEXPOSE:
+                print("MOVING WINDOW WILL CAUSE DESYNC")
+
 
 main_menu()
