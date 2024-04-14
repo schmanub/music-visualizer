96cdf28318b78b84a0b962d40bc2219f7518e307
Schmanub <asmanu21@gmail.com>
2024-04-14 15:08:32 -0400

    Added comments

--- graphics.py
+++ graphics.py
@@ -3,6 +3,7 @@ from numpy.fft import fft
 import numpy as np
 
 
+# function that maps ranges
 def map_range(range1: tuple, range2: tuple, value):
     slope = (range2[1] - range2[0]) / (range1[1] - range1[0])
     return range2[0] + slope * (value - range1[0])
@@ -19,20 +20,23 @@ class Waveform:
         xy = list()
         length = len(self.spect_data)
         interval = self.screen.get_width() / length
-        # rectangle drawing part
+        # create all points to draw lines between for wave form
         for i in range(0, length):
             value = map_range((-32768, 32767), (0, self.screen.get_height()), self.spect_data[i])
-            xy.append((interval*i, value))
+            xy.append((interval * i, value))
+        # actually draw them
         pg.draw.lines(self.screen, self.color, False, xy, 2)
 
     def histogram(self):
         space = 5
+        # calculating space between rectangle
         interval = self.screen.get_width() / self.rect_count
         interval2 = ((self.screen.get_width()) / (self.rect_count + space))
         # use fourier transforms to visualize the data
         data = abs(fft(self.spect_data, self.rect_count, -1, "ortho"))
         normalized_data = data / np.linalg.norm(data)
+        # actually draw the bars
         for i in range(0, self.rect_count):
-            height = normalized_data[i] * (self.screen.get_height()*8)
+            height = normalized_data[i] * (self.screen.get_height() * 8)
             rect = (i * (interval + space), self.screen.get_height() - height, interval2, height)
             pg.draw.rect(self.screen, self.color, rect)
--- main.py
+++ main.py
@@ -23,18 +23,20 @@ text_font = pg.font.SysFont("arial", 16)
 clock = pg.time.Clock()
 frame_rate = 60
 
-
+# function to draw text
 def draw_text(text, font, color, x, y):
     img = font.render(text, True, color)
     screen.blit(img, (x, y))
 
 
+# function that closes the file stream and quits the program
 def end(file_stream):
     file_stream.close()
     pg.quit()
     sys.exit()
 
 
+# function for generating RGB
 def rgb(time):
     # 3 cos waves all 120 degrees out of phase with each other
     x = time / 500
@@ -43,6 +45,7 @@ def rgb(time):
             int(255 * (0.5 * (math.cos(x + (math.pi * (4 / 3)))) + 0.5)))
 
 
+# converts a file stream into an array of PCM data
 def pcm_to_sound(file_stream):
     before = file_stream.tell()
     temp_data = np.frombuffer(file_stream.read(), dtype=np.int16)
@@ -50,6 +53,7 @@ def pcm_to_sound(file_stream):
     return pg.mixer.Sound(temp_data)
 
 
+# traps the program in a while true loop to keep everything paused
 def pause(file_stream):
     draw_text("Pause", text_font, "white", screen.get_width() - 60, 10)
     pg.display.update()
@@ -65,6 +69,7 @@ def pause(file_stream):
                     return music
 
 
+# helper function for calculating where to seek when skipping forwards and backwards
 def miliseek(time, block_size):
     fpmili = frame_rate / 1000
     pos = int((pg.time.get_ticks() - time) * fpmili * block_size)
@@ -79,6 +84,7 @@ def miliseek(time, block_size):
     return pos
 
 
+# main function that handles the main menu
 def main_menu():
     message_start = 0
     message = False
@@ -113,6 +119,7 @@ def main_menu():
         pg.display.update()
 
 
+# wav header decoding function
 def wav_header(input_file):
     # open the file in read only "r" binary mode "b"
     file_stream = open(input_file, "rb")
@@ -127,6 +134,7 @@ def wav_header(input_file):
         byte_rate = header_data[8]
         block_size = int(byte_rate / frame_rate)
         print(block_size)
+        # call the visualizer
         visualizer(file_stream, block_size)
     else:
         print("Unsupported wav file, only 16 bit wav files are supported")
@@ -134,6 +142,7 @@ def wav_header(input_file):
         main_menu()
 
 
+# MP3 decoding function, uses pydub
 def mp3_decoder(input_file):
     audio = AudioSegment.from_mp3(input_file)
     pcm_data = audio.raw_data
@@ -142,10 +151,13 @@ def mp3_decoder(input_file):
     pcm_file.seek(0)
     byte_rate = audio.frame_rate * audio.channels * 2
     block_size = int(byte_rate / frame_rate)
+    # call the visualizer
     visualizer(pcm_file, block_size)
 
 
+# code that handles the actual visualizer graphics and IO
 def visualizer(file_stream, block_size):
+    # initialize local variables
     file_stream.seek(0, 2)
     length = file_stream.tell()
     file_stream.seek(0)
@@ -157,32 +169,37 @@ def visualizer(file_stream, block_size):
     stat_time = 0
     music.play()
     stat_message = None
+    # main while loop
     while True:
+        # read a frame's worth of audio data
         data = file_stream.read(block_size)
+        # check if we reached the end of the file
         if len(data) < block_size:
             file_stream.close()
             main_menu()
+        # decode audio data
         decoded_data = np.frombuffer(data, dtype=np.int16)
         screen.fill((0, 0, 0))
+        # draw the stat messages if they need to be drawn
         if stat_message is not None:
             draw_text(stat_message, text_font, "white", 10, 10)
             if stat_time < pg.time.get_ticks():
                 stat_message = None
-        if len(decoded_data) == 0:
-            file_stream.close()
-            main_menu()
+        # prepare graphics library
         wave_form = graphics.Waveform(screen, decoded_data, rgb(pg.time.get_ticks()))
         match graphic_screen:
             case 0:
                 wave_form.histogram()
             case 1:
                 wave_form.line()
+        # py game event handling
         clock.tick(frame_rate)
         pg.display.update()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 end(file_stream)
             if event.type == pg.KEYDOWN:
+                # volume controls
                 if event.key == pg.K_DOWN and vol > 0:
                     vol -= 0.05
                     stat_message = "-Vol: " + str(round(vol*100)) + "%"
@@ -191,15 +208,18 @@ def visualizer(file_stream, block_size):
                     vol += 0.05
                     stat_message = "+Vol: " + str(round(vol*100)) + "%"
                     stat_time = pg.time.get_ticks() + 1000
+                # graphic mode switching
                 if event.key == pg.K_m:
                     if graphic_screen == 0:
                         graphic_screen = 1
                     else:
                         graphic_screen = 0
+                # pausing
                 if event.key == pg.K_SPACE:
                     music.stop()
                     music = pause(file_stream)
                     music.play()
+                # seeking left and right
                 if event.key == pg.K_LEFT and time - inc > 0:
                     time -= inc
                     stat_message = "<time: " + str(round(file_stream.tell() / length * 100)) + "%"
@@ -208,17 +228,20 @@ def visualizer(file_stream, block_size):
                     time += inc
                     stat_message = ">time: " + str(round(file_stream.tell() / length * 100)) + "%"
                     stat_time = pg.time.get_ticks() + 1000
+                # additional seeking code
                 if event.key == pg.K_RIGHT or event.key == pg.K_LEFT and time - inc > 0:
                     pos = miliseek(pg.time.get_ticks() - time, block_size)
                     file_stream.seek(pos)
                     music.stop()
                     music = pcm_to_sound(file_stream)
                     music.play()
-
+            # when the window is moved we need to make sure were still in sync with the audio
+            # since Windows will not run the program while its being moved but audio will keep playing
             if event.type == pg.VIDEOEXPOSE:
                 pos = miliseek(pg.time.get_ticks() - time, block_size)
                 file_stream.seek(pos)
         music.set_volume(vol)
 
 
+# start the program
 main_menu()
